54c08705f0723bc4d1f1907b554dfc806f683a31
Donna Torres <todonnatorres@icloud.com>
2021-02-04 16:03:04 -0600

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeOfDay){
+  const hour = parseInt(timeOfDay.split(":"));
+
+  if (hour < 12){
+    return "Good Morning";
+  }
+  else if (hour >= 12 && hour <= 17){
+    return "Good Afternoon";
+  }
+  else (hour > 17)
+  return "Good Evening"
+};
 /* Write your implementation of displayMessage() */
+function displayMessage(string){
+  document.getElementById('greeting').innerText = string
+}
+
+/* Notes: Functions can be assigned to variables:
+const sayHello = () => {
+  return "Hello";
+};*/
